ae88815bb82a523ce737c2bd923280b1b0e83141
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-01 21:13:00 +0700

    Create Set2.java

---
+++ Pertemuan 2/Set2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Set2 {
+     public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        System.out.println("   /\\       /\\");
+        System.out.println("  /  \\_____/  \\");
+        System.out.println(" /             \\");
+        System.out.println("(  /\\       /\\  )");
+        System.out.println("====    v    ====");
+        System.out.println("======(_|_)======");
+        System.out.println("  (           )");
+        System.out.println("   (_________)");
+        
+    } 
+}
