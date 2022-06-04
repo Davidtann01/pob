413aa123fe60180e66537e48fadf50981aab710e
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 20:23:09 +0700

    Create Prisioner1.java

---
+++ Pertemuan 7/Prisioner1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prisionertest;
+
+/**
+ *
+ * @author USER
+ */
+public class Prisioner {
+   public string name;
+public double height;
+public int sentence;
+
+public void think(){
+System.out.println("I'll have my revenge.");
+}//end method think
+}//end class Prisioner
