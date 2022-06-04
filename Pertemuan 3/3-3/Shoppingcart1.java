168d9ad35eb0e9bf85d2412582d4d1b4a83bf409
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 19:29:24 +0700

    Create Shoppingcart1.java

---
+++ Pertemuan 3/3-3/Shoppingcart1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Shoppingcart1 {
+     public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "Luis";
+        String itemDesc = "Momo";
+        String message;
+        
+        
+        // Assign the message variable 
+        message = custName + " mau membeli " + itemDesc;
+        
+        // Print and run the code
+        System.out.println(message);
+    }
+}
