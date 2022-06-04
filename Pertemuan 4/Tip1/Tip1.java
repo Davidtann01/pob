d082f97b4d47a9596179f79e136e757d6121435f
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 19:39:51 +0700

    Create Tip1.java

---
+++ Pertemuan 4/Tip1/Tip1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Tip1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        //Find everyone's indivudlal total after tax(5%) and tip(15%)
+        
+        /*This is what everyone owes before tax and tip:
+        Person 1: $10
+        Person 2: $12
+        Person 3: $9
+        Person 4: $8
+        Person 5: $7
+        Person 6: $15
+        Person 7: $11
+        Person 8: $30
+        */
+        
+    }    
+    }
+    
+}
