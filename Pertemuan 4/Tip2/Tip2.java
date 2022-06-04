6709277bf533686842b8ccbf29af9a6c902595f0
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 19:40:03 +0700

    Create Tip2.java

---
+++ Pertemuan 4/Tip2/Tip2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Tip2 {
+    public class Calculator {
+    public double tax = .05;
+    public double tip = .15;  //Change tax and tip if you prefer different values
+    public double originalPrice = 0;
+    
+    public void findTotal(){
+        //Calculate an individual's total after tax and tip
+        double total = originalPrice * (1+tax+tip);
+        //Print this value
+        System.out.println(total);
+        
+        
+    }
+}
