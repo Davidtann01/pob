cc7f1f8b64f587b69fc911d06e3452dcd4ca4a95
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 20:23:15 +0700

    Create Prison2.java

---
+++ Pertemuan 7/Prison2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Prison2 {
+        
+    public static final int UMUR = 10;
+    public int tinggi;
+    
+    public void tampilkan(){
+        System.out.println(UMUR);
+        System.out.println(tinggi);
+    }
+
+    public static void nama(String name){
+        System.out.println("nama: "+name);}
+}
