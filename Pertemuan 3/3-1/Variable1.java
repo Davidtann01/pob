1c07613d348ec7e0d1b73cf266cf1a41ee18af1d
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 19:29:05 +0700

    Create Variable1.java

---
+++ Pertemuan 3/3-1/Variable1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Variable1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String x = "Enka";
+        System.out.println("Hai!!! Namaku  " +x);
+        System.out.println(x +" keren!");
+        System.out.println("Yo, " +x +"!");
+        System.out.println("haloooo " +x +" "
+                + "Yo! My name is " +x +". I know how excited "
+                + "everyone is to start appreciating " +x +" on " +x
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay "
+                + x +"!!! That's me! " +x +" is the best date ever!");
+        
+        String nama = "Susan";
+        nama = "yena";
+        nama = "Luis";
+        
+        //"Yuki" = nama; // can not do this
+        System.out.println(nama);
+        
+    }
+    
+}
