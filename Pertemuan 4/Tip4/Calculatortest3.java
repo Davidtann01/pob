329e7c5a2eeb97b470eccc8f8f21c5dcf9cb0b2a
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 19:40:16 +0700

    Create Calculatortest3.java

---
+++ Pertemuan 4/Tip4/Calculatortest3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Calculatortest3 {
+   public static void main(String[] args) {
+        
+        Calculator calc = new Calculator();
+        
+        //Name your friends
+        calc.findTotal(10, null);
+        calc.findTotal(12, null);
+        calc.findTotal(9,  null);
+        calc.findTotal(8,  null);
+        calc.findTotal(7,  null);
+        calc.findTotal(15, "Alex");
+        calc.findTotal(11, null);
+        calc.findTotal(30, "Forgetful");
+
+        //Find and print the entire table's total, including tax and tip
+       
+    }  
+}
