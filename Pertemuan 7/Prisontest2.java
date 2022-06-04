cf9674efe2e0848ed58e6c5c70214b0e71624d60
Davidtann01 <97678783+Davidtann01@users.noreply.github.com>
2022-06-04 20:23:21 +0700

    Create Prisontest2.java

---
+++ Pertemuan 7/Prisontest2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Prisontest2 {
+      Prisoner.nama("buba");
+
+        Prisoner buba = new Prisoner();
+        buba.tinggi = 180;
+        buba.tampilkan();
+
+    }
+}
